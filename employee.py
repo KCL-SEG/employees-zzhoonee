ab1fa6aa8efdf2c8d5cd006c2c6d81c7a99360f6
JH Park <59407771+zzhoonee@users.noreply.github.com>
2023-09-26 20:08:32 +0100

    Update employee.py
    Initial commit

--- employee.py
+++ employee.py
@@ -1,31 +1,91 @@
 """Employee pay calculator."""
 """ENTER YOUR SOLUTION HERE!"""
 
+
 class Employee:
     def __init__(self, name):
         self.name = name
+        self.salary_pay = 0
+        self.hourly_pay = 0
+        self.hours_worked = 0
+        self.comm_pay = 0
+        self.no_of_contracts = 0
+        self.bonus_pay = 0
+
+    def set_salary(self, salary_pay):
+        self.salary_pay = salary_pay
+
+    def set_hourly(self, hourly_pay, hours_worked):
+        self.hourly_pay = hourly_pay
+        self.hours_worked = hours_worked
+
+    def set_comm(self, comm_pay, no_of_contracts=0):
+        self.comm_pay = comm_pay
+        self.no_of_contracts = no_of_contracts
+
+    def set_bonus(self, bonus_pay):
+        self.bonus_pay = bonus_pay
+
+    def get_salary(self):
+        return self.salary_pay + self.comm_pay * self.hourly_pay
+
+    def get_comm(self):
+        return self.comm_pay * self.no_of_contracts + self.bonus_pay
 
     def get_pay(self):
-        pass
+        salary_total = self.salary_pay
+        hourly_total = self.hourly_pay * self.hours_worked
+        comm_total = self.comm_pay * self.no_of_contracts
+        bonus_total = self.bonus_pay
+
+        return salary_total + hourly_total + comm_total + bonus_total
 
     def __str__(self):
-        return self.name
+        employee = f"{self.name} works on a "
+
+        if self.salary_pay > 0:
+            employee += f"monthly salary of {self.salary_pay} "
+        elif self.hourly_pay > 0 and self.hours_worked > 0:
+            employee += f"contract of {self.hours_worked} hours at {self.hourly_pay}/hour "
+
+        if self.comm_pay > 0:
+            employee += "and receives a commission for "
+            if self.no_of_contracts > 0:
+                employee += f"{self.no_of_contracts} contract(s) at {self.comm_pay}/contract "
+            else:
+                employee += f"a bonus commission of {self.comm_pay} "
+
+        employee += f". Their total pay is {self.get_pay()}."
+        return employee.strip()
 
 
 # Billie works on a monthly salary of 4000.  Their total pay is 4000.
 billie = Employee('Billie')
+billie.set_salary(4000)
 
 # Charlie works on a contract of 100 hours at 25/hour.  Their total pay is 2500.
 charlie = Employee('Charlie')
+charlie.set_hourly(25, 100)
 
-# Renee works on a monthly salary of 3000 and receives a commission for 4 contract(s) at 200/contract.  Their total pay is 3800.
+# Renee works on a monthly salary of 3000 and receives a commission for 4 contract(s) at 200/contract.  Their total
+# pay is 3800.
 renee = Employee('Renee')
+renee.set_salary(3000)
+renee.set_comm(200, 4)
 
-# Jan works on a contract of 150 hours at 25/hour and receives a commission for 3 contract(s) at 220/contract.  Their total pay is 4410.
+# Jan works on a contract of 150 hours at 25/hour and receives a commission for 3 contract(s) at 220/contract.  Their
+# total pay is 4410.
 jan = Employee('Jan')
+jan.set_hourly(25, 150)
+jan.set_comm(220, 3)
+
 
 # Robbie works on a monthly salary of 2000 and receives a bonus commission of 1500.  Their total pay is 3500.
 robbie = Employee('Robbie')
+robbie.set_salary(2000)
+robbie.set_bonus(1500)
 
 # Ariel works on a contract of 120 hours at 30/hour and receives a bonus commission of 600.  Their total pay is 4200.
 ariel = Employee('Ariel')
+ariel.set_hourly(30, 120)
+ariel.set_bonus(600)
